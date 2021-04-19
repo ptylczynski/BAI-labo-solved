8c468d98a01a7f74e4aa20ddcb2c87bd16ee44d9
Piotr Tylczynski <piotr@ptl.cloud>
2021-04-19 10:25:57 +0200

    car dto add

--- src/main/java/cloud/ptl/carmanager/car/CarDTO.java
+++ src/main/java/cloud/ptl/carmanager/car/CarDTO.java
@@ -1,4 +1,26 @@
 package cloud.ptl.carmanager.car;
 
+import cloud.ptl.carmanager.client.ClientDAO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class CarDTO {
+    private Long id;
+    private String numberPlate;
+    private String seatsNumber;
+    private Double totalDistance;
+
+    public CarDTO toDTO(CarDAO entity){
+        return CarDTO.builder()
+                .id(entity.getId())
+                .numberPlate(getNumberPlate())
+                .seatsNumber(entity.getSeatsNumber())
+                .totalDistance(entity.getTotalDistance())
+                .build();
+    }
 }
---
+++ src/main/java/cloud/ptl/carmanager/client/ClientDTO.java
@@ -0,0 +1,7 @@
+package cloud.ptl.carmanager.client;
+
+import lombok.Data;
+
+@Data
+public class ClientDTO {
+}
